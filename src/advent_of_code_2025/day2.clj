b096892f4b736c595504303628637384d6a0f49b
Helen Williamson <helen.williamson@triptease.com>
2025-12-17 22:05:32 +0000

    day-2 part 1

---
+++ src/advent_of_code_2025/day2.clj
@@ -0,0 +1,44 @@
+(ns advent-of-code-2025.day-2
+  (:gen-class)
+  (:require [advent-of-code-2025.utils :as utils]
+            [clojure.string :as string]))
+
+(defn parse-data
+  [name]
+  (->> (-> (utils/read-input name)
+          (first)
+          (string/split #","))
+       (map (fn [line] (let [[start end] (->> (string/split line #"-")
+                                   (map #(Long/parseLong %1)))]
+                         (range start (inc end)))))))
+(defn valid-id-multiple?
+  ([id]
+   (if (odd? (count id))
+     true
+     (let [total-sizes (/ (count id) 2)
+           sizes (range 1 (inc total-sizes))]
+       (every? #(valid-id? id %1) sizes))))
+  ([id size]
+   (let [digits (apply str (take size id))
+         number-to-take (/ (count id) size)
+         potential-id (apply str (take number-to-take (repeat digits)))]
+     (not= id potential-id))))
+
+(defn valid-id?
+  ([id]
+   (if (odd? (count id))
+     true
+     (let [size (/ (count id) 2)
+           digits (apply str (take size id))
+           number-to-take (/ (count id) size)
+           potential-id (apply str (take number-to-take (repeat digits)))]
+       (not= id potential-id)))))
+
+(defn part-1
+  [name]
+  (->> (parse-data name)
+       (flatten)
+       (map #(str %1))
+       (filter #(not (valid-id? %1)))
+       (map #(Long/parseLong %1))
+       (reduce +)))
